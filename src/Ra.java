9b8feff15a9788b9fcbb927b16d61b4d5058c871
zhangyunqi <zhang.yunqi@trans-cosmos.com.cn>
2020-09-14 16:33:14 +0800

    uu

--- src/Ra.java
+++ src/Ra.java
@@ -1,15 +1,8 @@
 public class Ra{
     public static void main(String[] args) {
         System.out.print("ddd");
-<<<<<<< HEAD
 		master
-		master
-		master
-=======
 		first
-		first
-		first
->>>>>>> remotes/origin/first
 		sfdsfdsfa
     }
 }
